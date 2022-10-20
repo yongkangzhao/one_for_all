108cda6f8fd55508b36f2534e502556bd8e219e5
kpjwong <kjw65@georgetown.edu>
2022-10-19 20:26:54 -0400

    Adding initializer for newly added seed lists.

--- generator/initialize_all.sh
+++ generator/initialize_all.sh
@@ -4,3 +4,5 @@ python initializer.py --collection occupation
 python initializer.py --collection perception
 python initializer.py --collection persona
 python initializer.py --collection skill
+python initializer.py --collection ncskills
+python initializer.py --collection traits
