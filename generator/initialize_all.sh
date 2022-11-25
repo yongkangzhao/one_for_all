223c64c59db0d0f3ac55601e3936fe9aa180461a
Aiden Zhao <cookieyong@gmail.com>
2022-11-24 19:40:15 -0800

    refactor and redesigned prompts and seeds

--- generator/initialize_all.sh
+++ generator/initialize_all.sh
@@ -1,10 +1,27 @@
-python initializer.py --collection motivation
+# concept
+# feel
+# fieldOfStudy 
+# job
+# motivation
+# ncskills 
+# perception
+# persona
+# personality
+# skill
+# tasks 
+# traits
+# occupation
+python initializer.py --collection concept
+python initializer.py --collection feel
 python initializer.py --collection fieldOfStudy
-python initializer.py --collection occupation
+python initializer.py --collection job
+python initializer.py --collection motivation
+python initializer.py --collection ncskills
 python initializer.py --collection perception
 python initializer.py --collection persona
+python initializer.py --collection personality
 python initializer.py --collection skill
-python initializer.py --collection ncskills
-python initializer.py --collection traits
-python initializer.py --collection values
 python initializer.py --collection tasks
+python initializer.py --collection traits
+python initializer.py --collection occupation
+
